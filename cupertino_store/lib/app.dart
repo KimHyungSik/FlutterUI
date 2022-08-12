0fc7886259bf5e8828cec0087abcb04ab2f62e2a
KimHyungSik <colrog968@gmail.com>
2022-08-12 09:23:53 +0900

    Create structure for a 3-tab app

---
+++ cupertino_store/lib/app.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  const CupertinoStoreApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return const CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
+
+class CupertinoStoreHomePage extends StatelessWidget {
+  const CupertinoStoreHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
+      ),
+      tabBuilder: (context, index) {
+        late final CupertinoTabView returnValue;
+        switch (index) {
+          case 0:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: ProductListTab(),
+              );
+            });
+            break;
+          case 1:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: SearchTab(),
+              );
+            });
+            break;
+          case 2:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: ShoppingCartTab(),
+              );
+            });
+            break;
+        }
+        return returnValue;
+      },
+    );
+  }
+}
+
+class ShoppingCartTab extends StatelessWidget {
+  const ShoppingCartTab({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, value, child) {
+        return const CustomScrollView(
+          slivers: <Widget>[
+            CupertinoSliverNavigationBar(
+              largeTitle: Text('Cupertino Store'),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
+
+class SearchTab extends StatefulWidget {
+  const SearchTab({Key? key}) : super(key: key);
+
+  @override
+  State<SearchTab> createState() => _SearchTabState();
+}
+
+class _SearchTabState extends State<SearchTab> {
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScrollView(
+      slivers: [
+        CupertinoSliverNavigationBar(
+          largeTitle: Text('Search'),
+        )
+      ],
+    );
+  }
+}
+
+class ProductListTab extends StatefulWidget {
+  const ProductListTab({Key? key}) : super(key: key);
+
+  @override
+  State<ProductListTab> createState() => _ProductListTabState();
+}
+
+class _ProductListTabState extends State<ProductListTab> {
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScrollView(
+      slivers: [
+        CupertinoSliverNavigationBar(
+          largeTitle: Text('Shopping Cart'),
+        )
+      ],
+    );
+  }
+}
